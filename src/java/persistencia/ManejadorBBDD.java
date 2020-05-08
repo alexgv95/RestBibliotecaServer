9cdc23d95908cec9e335452053087aa33272888c
aleja <aleja@Alex-PC>
2020-05-08 12:29:49 +0200

    auth conf

--- src/java/persistencia/ManejadorBBDD.java
+++ src/java/persistencia/ManejadorBBDD.java
@@ -5,6 +5,13 @@
  */
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 /**
  *
  * @author Alex <agutierrezvivancos@gmail.com>
@@ -12,7 +19,56 @@ package persistencia;
 public class ManejadorBBDD {
 
     public String registrarUsuario(String nombre, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String respuesta = "";
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rS = null;
+        try {
+            InitialContext initialContext = new InitialContext();
+            DataSource dataSource = (DataSource) initialContext.lookup("jdbc/biblioDatasource");
+            conn = dataSource.getConnection();
+            String query = "SELECT * FROM usuarios WHERE nombre='" + nombre + "' "
+                    + "and password=sha1('" + password + "');";
+            st = conn.createStatement();
+            rS = st.executeQuery(query);
+            if(rS.next()){
+                respuesta = "Usuario existente";
+            } else {
+                query = "INSERT INTO usuarios(nombre,password) VALUES('" + nombre
+                        + "',sha1('" + password + "'));";
+                st.executeQuery(query);
+                respuesta = "Se ha registra el usuario con éxito !";
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
+        } finally {
+            liberarRecursos(rS, st, conn);
+        }
+        return respuesta;
     }
-    
+
+    public static void liberarRecursos(ResultSet rS, Statement st, Connection conn) {
+        if (rS != null) {
+            try {
+                rS.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
 }
